83511ee65921335aecfc9ff950890eec714fc983
rumplestilzken <rumplestilzken@gmail.com>
2023-03-21 03:06:34 -0900

    Updated path for update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #phh treble updates
-rsync -r ~/git/lineageos_20_td/device/phh/treble/* /usr/local/lineage-20-build-td/device/phh/treble/
+rsync -r ~/git/lineageos_20_td/device/phh/treble/* ~/git/lineage-20-build-td/device/phh/treble/
 
 #apps
-rsync -r --mkpath ~/git/lineageos_20_td/packages/apps/* /usr/local/lineage-20-build-td/packages/apps/
+rsync -r --mkpath ~/git/lineageos_20_td/packages/apps/* ~/git/lineage-20-build-td/packages/apps/
 
 
 
