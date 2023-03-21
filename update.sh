76c5ba5cc1309ccf2b60f9978f9244e755307737
rumplestilzken <rumplestilzken@gmail.com>
2023-03-21 02:42:20 -0900

    Updated update.sh with recursive call for phh/treble

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #phh treble updates
 rsync -r ~/git/lineageos_20_td/device/phh/treble/* /usr/local/lineage-20-build-td/device/phh/treble/
-rsync -r ~/git/lineageos_20_td/device/phh/treble/twrp/* /usr/local/lineage-20-build-td/device/phh/treble/twrp/
-rsync -r ~/git/lineageos_20_td/device/phh/treble/remote/* /usr/local/lineage-20-build-td/device/phh/treble/remote/
-rsync -r ~/git/lineageos_20_td/device/phh/treble/fixSPL/* /usr/local/lineage-20-build-td/device/phh/treble/fixSPL/
 
 #apps
 rsync -r --mkpath ~/git/lineageos_20_td/packages/apps/* /usr/local/lineage-20-build-td/packages/apps/
